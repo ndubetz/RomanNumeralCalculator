50a0d0e0075d11b4d45fd0eb737144b30775cec9
ndub <ndubetz@pillartechnology.com>
2016-06-21 17:39:40 -0400

    started adding Makefiles. Not building yet.

--- test/check_RomanNumeralCalculator.c
+++ test/check_RomanNumeralCalculator.c
@@ -5,9 +5,17 @@
 
 START_TEST ("adds I and I to get II")
 {
-	char * i = "I";
-	char * ii = "II";
+	char * i      = "I";
+	char * ii     = "II";
 	char * result = RomanNumeralCalculator_add(i, i);
 	ck_assert_str_eq(result, ii);		
 }
 END_TEST
+
+START_TEST ("adds II to I to get III")
+{
+	char * i      = "I";
+	char * ii     = "II";
+	char * iii    = "III";
+	char * result = RomanNumeralCalculator_add(ii, i);
+}
