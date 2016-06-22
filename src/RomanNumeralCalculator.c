c024f46226d31d42dac5cbb0b2095010979a45ea
ndub <ndubetz@pillartechnology.com>
2016-06-22 13:01:02 -0400

    these files were suppose to be in the last commit

--- src/RomanNumeralCalculator.c
+++ src/RomanNumeralCalculator.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
+#include <string.h>
 
-char * RomanNumeralCalculator_add(char * a, char * b)
+void apply_subtraction(char * sum)
 {
-	return "";
+    sum[strlen(sum) - 3] = 'V';
+    sum[strlen(sum) - 2] = NULL;    
+}
+
+char * RomanNumeralCalculator_add(char * number1, char * number2)
+{
+    char * sum = calloc(strlen(number1) + strlen(number2) + 1, sizeof(char));
+    strcpy(sum, number1);
+    strcat(sum, number2);    
+
+    if(strlen(sum) > 3)
+    {
+        apply_subtraction(sum);
+    }
+
+	return sum;
 }
--- src/RomanNumeralCalculator.h
+++ src/RomanNumeralCalculator.h
@@ -1,3 +1,3 @@
 
 
-char * RomanNumeralCalculator_add(char * a, char* b);
+char * RomanNumeralCalculator_add(char * number1, char * number2);
