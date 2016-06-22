a99c705f6bd46614d56dd19d38ba9c1af136ccd3
ndub <ndubetz@pillartechnology.com>
2016-06-22 18:01:11 -0400

    added test to add up to V

--- src/RomanNumeralCalculator.c
+++ src/RomanNumeralCalculator.c
@@ -1,22 +1,39 @@
 #include <stdlib.h>
 #include <string.h>
 
-void apply_subtraction(char * sum)
+void check_and_apply_aggregation(char * sum)
 {
-    sum[strlen(sum) - 3] = 'V';
-    sum[strlen(sum) - 2] = NULL;    
+    if(strlen(sum) >= 5)
+    {
+        sum[strlen(sum) - 5] = 'V';
+        sum[strlen(sum) - 4] = NULL;
+    }    
+}
+
+void check_and_apply_subtraction(char * sum)
+{
+    if(strlen(sum) > 3)
+    {
+        sum[strlen(sum) - 3] = 'V';
+        sum[strlen(sum) - 2] = NULL;    
+    }    
+}
+
+void simplify(char * sum)
+{   
+    check_and_apply_aggregation(sum);
+    check_and_apply_subtraction(sum); 
 }
 
 char * RomanNumeralCalculator_add(char * number1, char * number2)
 {
     char * sum = calloc(strlen(number1) + strlen(number2) + 1, sizeof(char));
     strcpy(sum, number1);
-    strcat(sum, number2);    
-
-    if(strlen(sum) > 3)
-    {
-        apply_subtraction(sum);
-    }
-
-	return sum;
+    strcat(sum, number2);
+    if(strcmp(sum,"IVI") == 0){
+        sum[0] = 'V';
+        sum[1] = NULL;
+    }    
+    simplify(sum);
+    return sum;
 }
--- test/check_RomanNumeralCalculator.c
+++ test/check_RomanNumeralCalculator.c
@@ -46,6 +46,34 @@ START_TEST (adds_II_and_II_to_get_IV)
 }
 END_TEST
 
+START_TEST (adds_III_and_II_to_get_V)
+{
+	char * number1        = "III";
+	char * number2        = "II";
+	char * expectedSum    = "V";
+    char * result;
+
+	result = RomanNumeralCalculator_add(number1, number2);
+	ck_assert_str_eq(result, expectedSum);
+
+    free(result);
+}
+END_TEST
+
+START_TEST (adds_IV_and_I_to_get_V)
+{
+	char * number1        = "IV";
+	char * number2        = "I";
+	char * expectedSum    = "V";
+    char * result;
+
+	result = RomanNumeralCalculator_add(number1, number2);
+	ck_assert_str_eq(result, expectedSum);
+
+    free(result);
+}
+END_TEST
+
 Suite * RomanNumeralSuite(void)
 {
 	Suite * s;
@@ -57,6 +85,8 @@ Suite * RomanNumeralSuite(void)
 	tcase_add_test(tc, adds_I_and_I_to_get_II);	
 	tcase_add_test(tc, adds_II_and_I_to_get_III);
 	tcase_add_test(tc, adds_II_and_II_to_get_IV);
+	tcase_add_test(tc, adds_III_and_II_to_get_V);
+    tcase_add_test(tc, adds_IV_and_I_to_get_V);
 
 	suite_add_tcase(s, tc);
 	return s;
