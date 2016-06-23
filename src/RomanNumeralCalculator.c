3e38091c23167af50e981311469ef19d563df045
ndub <ndubetz@pillartechnology.com>
2016-06-22 23:09:44 -0400

    refactor and cleanup

--- src/RomanNumeralCalculator.c
+++ src/RomanNumeralCalculator.c
@@ -1,39 +1,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-void check_and_apply_aggregation(char * sum)
-{
-    if(strlen(sum) >= 5)
+char * decompose(char * sum)
+{            
+    int i;
+    int length = strlen(sum);
+    char * result;
+    for(i = 0; i < length; i++)
     {
-        sum[strlen(sum) - 5] = 'V';
-        sum[strlen(sum) - 4] = NULL;
+        if(sum[i] == 'V')
+        {
+            char * IIII = "IIII";
+            char * left = calloc(i + 1, sizeof(char)); 
+            char * right = calloc(length - i, sizeof(char));
+            char * result = calloc(length + 3, sizeof(char));
+            strncpy(left, sum, i - 1);
+            strncpy(right, sum + i + 1, length - i + 1);
+            strcpy(result, left);
+            strcat(result, IIII);
+            strcat(result, right);
+            return result;
+        }
     }    
-}
-
-void check_and_apply_subtraction(char * sum)
-{
-    if(strlen(sum) > 3)
-    {
-        sum[strlen(sum) - 3] = 'V';
-        sum[strlen(sum) - 2] = NULL;    
-    }    
+    return sum;
 }
 
 void simplify(char * sum)
 {   
-    check_and_apply_aggregation(sum);
-    check_and_apply_subtraction(sum); 
+    if(strcmp(sum, "IIII") == 0)
+    {
+        sum[1] = 'V';
+        sum[2] = NULL;
+    }
+    if(strcmp(sum, "IIIII") == 0)
+    {
+        sum[0] = 'V';
+        sum[1] = NULL;    
+    }
 }
 
 char * RomanNumeralCalculator_add(char * number1, char * number2)
 {
-    char * sum = calloc(strlen(number1) + strlen(number2) + 1, sizeof(char));
+    char * sum = calloc(21, sizeof(char));
     strcpy(sum, number1);
     strcat(sum, number2);
-    if(strcmp(sum,"IVI") == 0){
-        sum[0] = 'V';
-        sum[1] = NULL;
-    }    
+    sum = decompose(sum);    
     simplify(sum);
     return sum;
 }
