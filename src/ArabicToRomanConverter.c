383e0b4cf8694c65e3094d179dc59b87d9eded0f
NDUB <ndubetz@pillartechnology.com>
2016-06-24 16:25:44 -0400

    added tests for converting to subtractive roman numerals

--- src/ArabicToRomanConverter.c
+++ src/ArabicToRomanConverter.c
@@ -3,21 +3,71 @@
 
 char * ArabicToRomanConverter_convert(int arabic_number)
 {
-    switch(arabic_number)
-    {
-        case 1000:
-            return "M";
-        case 500:
-            return "D";
-        case 100:
-            return "C";
-        case 50:
-            return "L";
-        case 10:
-            return "X";
-        case 5:
-            return "V";
-        case 1:
-            return "I";    
-    }
+    char * roman_numeral = calloc(15, sizeof(char));
+        if(arabic_number >= 1000)
+        {
+            strcat(roman_numeral, "M");
+            arabic_number -= 1000;
+        }
+        else if(arabic_number >= 900)
+        {
+            strcat(roman_numeral, "CM");
+            arabic_number -= 900;
+        }
+        else if(arabic_number >= 500)
+        {
+            strcat(roman_numeral, "D");
+            arabic_number -= 500;
+        }
+        else if(arabic_number >= 400)
+        {
+            strcat(roman_numeral, "CD");
+            arabic_number -= 500;
+        }
+        else if(arabic_number >= 100)
+        {
+            strcat(roman_numeral, "C");
+            arabic_number -= 100;
+        }
+        else if(arabic_number >= 90)
+        {
+            strcat(roman_numeral, "XC");
+            arabic_number -= 100;
+        }
+        else if(arabic_number >= 50)
+        {
+            strcat(roman_numeral, "L");
+            arabic_number -= 50;
+        }
+        else if(arabic_number >= 40)
+        {
+            strcat(roman_numeral, "XL");
+            arabic_number -= 40;
+        }   
+        else if(arabic_number >= 10)
+        {
+            strcat(roman_numeral, "X");
+            arabic_number -= 10;
+        }        
+        else if(arabic_number >= 9)
+        {
+            strcat(roman_numeral, "IX");
+            arabic_number -= 9;
+        }
+        else if(arabic_number >= 5)
+        {
+            strcat(roman_numeral, "V");
+            arabic_number -= 5;
+        }
+        else if(arabic_number >= 4)
+        {
+            strcat(roman_numeral, "IV");
+            arabic_number -= 4;
+        }
+        else
+        {
+            strcat(roman_numeral, "I");
+            arabic_number -= 1;
+        }    
+
 }
--- test/check_ArabicToRomanConverter.c
+++ test/check_ArabicToRomanConverter.c
@@ -16,10 +16,22 @@ START_TEST (convert_single_digits)
 }
 END_TEST
 
+START_TEST (convert_subtractive_numerals)
+{
+	ck_assert_str_eq("IV", ArabicToRomanConverter_convert(4));		
+    ck_assert_str_eq("IX", ArabicToRomanConverter_convert(9));
+    ck_assert_str_eq("XL", ArabicToRomanConverter_convert(40));
+    ck_assert_str_eq("XC", ArabicToRomanConverter_convert(90));
+    ck_assert_str_eq("CD", ArabicToRomanConverter_convert(400));
+    ck_assert_str_eq("CM", ArabicToRomanConverter_convert(900));
+}
+END_TEST
+
 TCase * ArabicToRomanConverterTests(void)
 {
 	TCase * tc;
 	tc = tcase_create("ArabicToRomanConverterTests");
 	tcase_add_test(tc, convert_single_digits);
+	tcase_add_test(tc, convert_subtractive_numerals);
 	return tc;
 }
