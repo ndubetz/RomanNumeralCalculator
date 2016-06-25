674e9196ef4ba2b174ac03e0e6e761f121efb0a6
NDUB <ndubetz@pillartechnology.com>
2016-06-24 21:44:24 -0400

    added tests for additive numerals for RomanToArabicConverter

--- src/RomanToArabicConverter.c
+++ src/RomanToArabicConverter.c
@@ -3,14 +3,32 @@
 
 #include "RomanToArabic.h"
 
+int convert_numeral(char numeral)
+{
+    const char numeral_as_string[] = {numeral, '\0'};
+	int i;
+	for (i = 0; i < ROMAN_TO_ARABIC_LENGTH; i++)
+	{
+		if(strcmp(numeral_as_string, ROMAN_TO_ARABIC[i].roman) == 0)
+		{
+			return ROMAN_TO_ARABIC[i].arabic;
+		}
+	}
+}
+
 int RomanToArabicConverter_convert(char * roman_numeral)
 {
+	int arabic_number = 0;
+    int previous_value = 0;
     int i;
-    for(i = 0; i < ROMAN_TO_ARABIC_LENGTH; i++)
+    for(i = strlen(roman_numeral); i-- ;)
     {
-        if(strcmp(roman_numeral, ROMAN_TO_ARABIC[i].roman) == 0)
-        {
-            return ROMAN_TO_ARABIC[i].arabic;
-        }
+    	int digit_value = convert_numeral(roman_numeral[i]);
+        arabic_number += (digit_value >= previous_value) 
+        	?  digit_value 
+        	: -digit_value;
+        previous_value = digit_value;
     }
+    return arabic_number;
 }
+
--- test/check_RomanToArabicConverter.c
+++ test/check_RomanToArabicConverter.c
@@ -27,11 +27,31 @@ START_TEST (convert_subtractive_numerals)
 }
 END_TEST
 
+START_TEST (convert_additive_numerals)
+{
+    ck_assert_int_eq(RomanToArabicConverter_convert("III"), 3);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("VIII"), 8);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("XI"), 11);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("XXVII"), 27);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("LV"), 55);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("LXXVI"), 76);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("CXI"), 111);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("CCCLX"), 360);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("DCCLXXVII"), 777);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("MXXII"), 1022);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("MDCCCXII"), 1812);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("MMCI"), 2101);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("MMDCLXVI"), 2666);		
+    ck_assert_int_eq(RomanToArabicConverter_convert("MMMCCCXXXIII"), 3333);		
+}
+END_TEST
+
 TCase * RomanToArabicConverterTests(void)
 {
 	TCase * tc;
 	tc = tcase_create("RomanToArabicConverterTests");
 	tcase_add_test(tc, convert_single_numerals);
 	tcase_add_test(tc, convert_subtractive_numerals);
+	tcase_add_test(tc, convert_additive_numerals);
 	return tc;
 }
